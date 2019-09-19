edb80452263adff4a7f5457ff05d3c1bff647678
Ahnaf-41M <mdahnaf7@gmail.com>
2019-09-19 21:42:00 +0600

    Add solution for problem `C - Perfect Team` Link: https://codeforces.com/contest/1221/problem/C Tags: binary search, math, *1200 Ref: https://codeforces.com/contest/1221/submission/60876496

--- codeforces/1221/C.cpp
+++ codeforces/1221/C.cpp
@@ -1,16 +1,16 @@
-    #include<bits/stdc++.h>
-    using namespace std;
-    int main()
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n,c,m,x,sum;
+    cin>>t;
+    while(t--)
     {
-        int t,n,c,m,x,sum;
-        cin>>t;
-        while(t--)
-        {
-            cin>>c>>m>>x;
-            sum=(c+m+x)/3;
-            if(sum>min(c,m))
-                cout<<min(c,m)<<endl;
-            else
-                cout<<sum<<endl;
-        }
+        cin>>c>>m>>x;
+        sum=(c+m+x)/3;
+        if(sum>min(c,m))
+            cout<<min(c,m)<<endl;
+        else
+            cout<<sum<<endl;
     }
+}
