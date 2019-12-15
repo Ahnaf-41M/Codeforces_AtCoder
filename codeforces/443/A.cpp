bbba08f9ceb8ec0395268188fcbf1cc9957fd558
Ahnaf-41M <mdahnaf7@gmail.com>
2019-12-15 20:58:00 +0600

    Add solution for problem `A - Anton and Letters` Link: https://codeforces.com/contest/443/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/443/submission/66976768

--- codeforces/443/A.cpp
+++ codeforces/443/A.cpp
@@ -1,26 +1,26 @@
-        #include<bits/stdc++.h>
-        using namespace std;
-         
-        int main()
+    #include<bits/stdc++.h>
+    using namespace std;
+     
+    int main()
+    {
+        int a[27];
+        for(int i = 0;i < 27; i++)
         {
-            int a[27];
-            for(int i = 0;i < 27; i++)
-            {
-                a[i]=0;
-            }
-            string s;
-            int sum=0;
-         
-            getline(cin,s,'\n');
-            for(int i = 0; s[i]!='\0'; i++)
-            {
-                if(s[i]>='a' && s[i]<='z')
-                {
-                    if(a[s[i]-97]==0){
-                    a[s[i]-97]++;
-                    sum++;}
-                }
-            }
-            cout<<sum<<endl;
-         
+            a[i]=0;
         }
+        string s;
+        int sum=0;
+     
+        getline(cin,s,'\n');
+        for(int i = 0; s[i]!='\0'; i++)
+        {
+            if(s[i]>='a' && s[i]<='z')
+            {
+                if(a[s[i]-97]==0){
+                a[s[i]-97]++;
+                sum++;}
+            }
+        }
+        cout<<sum<<endl;
+     
+    }
