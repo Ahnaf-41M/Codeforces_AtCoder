18456767bffd9808ac66a6e7657cbc66603eeb3d
Ahnaf-41M <mdahnaf7@gmail.com>
2020-03-01 17:05:00 +0600

    Add solution for problem `B - Interesting drink` Link: https://codeforces.com/contest/706/problem/B Tags: binary search, dp, implementation, *1100 Ref: https://codeforces.com/contest/706/submission/72156968

--- codeforces/706/B.cpp
+++ codeforces/706/B.cpp
@@ -1,31 +1,31 @@
-    #include<bits/stdc++.h>
-    using namespace std;
-     
-    #define ll long long
-     
-     
-    int main()
-    {
-         ll i,j,q,t,n;
-         ios_base::sync_with_stdio(false);
-         cin.tie(NULL);
-         cin>>n;
-     
-         ll x[n+1];
-         for(i = 0; i < n; i++)
-              cin>>x[i];
-     
-         sort(x,x+n);
-     
-         cin>>q;
-         ll m[q+1];
-         for(i = 0; i < q; i++)
-         {
-              cin>>m[i];
-              //a[i]=b_search(m[i],x,n);
-         }
-         for(i = 0; i < q; i++)
-         {
-              cout<<upper_bound(x,x+n,m[i])-x<<endl;
-         }
-    }
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+
+int main()
+{
+     ll i,j,q,t,n;
+     ios_base::sync_with_stdio(false);
+     cin.tie(NULL);
+     cin>>n;
+
+     ll x[n+1];
+     for(i = 0; i < n; i++)
+          cin>>x[i];
+
+     sort(x,x+n);
+
+     cin>>q;
+     ll m[q+1];
+     for(i = 0; i < q; i++)
+     {
+          cin>>m[i];
+          //a[i]=b_search(m[i],x,n);
+     }
+     for(i = 0; i < q; i++)
+     {
+          cout<<upper_bound(x,x+n,m[i])-x<<endl;
+     }
+}
