2aa58103dd47ae3a804c883f41563293bcb50ca3
Ahnaf-41M <mdahnaf7@gmail.com>
2020-03-22 18:57:00 +0600

    Add solution for problem `C - Maximum Volume` Link: https://atcoder.jp/contests/abc159/tasks/abc159_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc159/submissions/11120332

---
+++ atcoder/abc159/C.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+     int l;
+     double ans;
+
+     cin>>l;
+     ans = 1.0*(l*1.0/3.0);
+     ans = ans*ans*ans;
+     printf("%.12lf\n",ans);
+}
