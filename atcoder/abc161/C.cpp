382414bab9eced8e7d5eb17c2bd169bdee52a2fa
Ahnaf-41M <mdahnaf7@gmail.com>
2020-04-04 18:34:00 +0600

    Add solution for problem `C - Replacing Integer` Link: https://atcoder.jp/contests/abc161/tasks/abc161_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc161/submissions/11523617

--- atcoder/abc161/C.cpp
+++ atcoder/abc161/C.cpp
@@ -1,24 +1,24 @@
-    #include<bits/stdc++.h>
-    using namespace std;
-    #define ll long long
-    int main()
-    {
-         ll n,k;
-     
-         cin>>n>>k;
-     
-         if(n%k==0)
-              cout<<0<<endl;
-         else if(n<k)
-         {
-              if((n%k)<(k-n))
-                   cout<<n<<endl;
-              else
-                   cout<<abs(n-k)<<endl;
-         }
-         else
-         {
-              n = n%k;
-              cout<<k-n<<endl;
-         }
-    }
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+     ll n,k;
+
+     cin>>n>>k;
+
+     if(n%k==0)
+          cout<<0<<endl;
+     else if(n<k)
+     {
+          if((n%k)<(k-n))
+               cout<<n<<endl;
+          else
+               cout<<abs(n-k)<<endl;
+     }
+     else
+     {
+          n = n%k;
+          cout<<k-n<<endl;
+     }
+}
