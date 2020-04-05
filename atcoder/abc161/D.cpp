104c45bcb3f28e011b053a168102a8528d608313
Ahnaf-41M <mdahnaf7@gmail.com>
2020-04-05 08:40:00 +0600

    Add solution for problem `D - Lunlun Number` Link: https://atcoder.jp/contests/abc161/tasks/abc161_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc161/submissions/11564174

--- atcoder/abc161/D.cpp
+++ atcoder/abc161/D.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 #define ll long long
-queue<ll>m;
+queue<ll>q;
 
 int main()
 {
     int k;
     cin >> k;
     for (int i = 1; i <= 9; i++)
-        m.push(i);
+        q.push(i);
 
     int j = 1;
 
     while (j < k)
     {
-        ll p = m.front();
-        m.pop();
+        ll p = q.front();
+        q.pop();
         j++;
 
         ll u = p % 10;
 
         if (u != 0)
-            m.push(p * 10 + u - 1);
+            q.push(p * 10 + u - 1);
 
-        m.push(p * 10 + u);
+        q.push(p * 10 + u);
 
         if (u != 9)
-            m.push(p * 10 + u + 1);
+            q.push(p * 10 + u + 1);
     }
-    cout << m.front() << endl;
+    cout << q.front() << endl;
 }
