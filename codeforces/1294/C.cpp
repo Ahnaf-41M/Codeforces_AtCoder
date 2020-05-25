b731fdf6967a79b988ef03105ed9a2e16d47784f
Ahnaf-41M <mdahnaf7@gmail.com>
2020-05-25 13:09:00 +0600

    Add solution for problem `C - Product of Three Numbers` Link: https://codeforces.com/contest/1294/problem/C Tags: greedy, math, number theory, *1300 Ref: https://codeforces.com/contest/1294/submission/81356663

--- codeforces/1294/C.cpp
+++ codeforces/1294/C.cpp
@@ -1,66 +1,66 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-
-#define  pb            push_back
-#define  int           long long
-#define  endl          "\n"
-#define  MX            100005
-
-#define  all(v)        v.begin(),v.end()
-#define  gcd(a,b)      __gcd(a,b)
-#define  lcm(a,b)      (a*b)/gcd(a,b)
-#define  rep(i,a,b)    for(int i = a; i <= b; i++)
-#define  irep(i,b,a)   for(int i = b; i >= a; i--)
-
-using namespace std;
-using namespace __gnu_cxx;
-
-int n;
-
-void Find_Divs(vector<int> &divs)
-{
-   for (int i = 2; i * i <= n; i++) {
-      if (n % i == 0) {
-         divs.pb(i);
-         if (n / i != i) divs.pb(n / i);
-      }
-   }
-   sort(all(divs));
-}
-void Solve()
-{
-   vector<int> divs;
-   cin >> n;
-
-   Find_Divs(divs);
-   for (int i = 0; i < divs.size(); i++) {
-      for (int j = i + 1; j < divs.size(); j++) {
-         int cur = divs[i] * divs[j];
-         if (cur > n)
-            break;
-         int d3 = n / cur;
-         if (d3 != divs[i] && d3 != divs[j] && d3 != 1
-               && d3 * divs[i]*divs[j] == n) {
-            cout << "YES\n";
-            cout << divs[i] << " " << divs[j] << " " << d3 << "\n";
-            return;
-         }
-      }
-   }
-   cout << "NO\n";
-   return;
-}
-signed main()
-{
-   ios_base::sync_with_stdio(0);
-   cin.tie(0);
-   cout.tie(0);
-
-   int T;
-   cin >> T;
-
-   while (T--)
-      Solve();
-
-   return 0;
-}
+    #include<bits/stdc++.h>
+    using namespace std;
+     
+     
+    #define ll long long
+    ll a[100000];
+    int main()
+    {
+        ll i,n,t,k=0,c=0,j,g=0;
+        vector<ll> v;
+     
+     
+        cin>>t;
+     
+        while(t--)
+        {
+            cin>>n;
+            a[1]=0,a[0]=0,a[2]=0;
+            for(i = 2; i <= sqrt(n); i++)
+            {
+                if(n%i==0)
+                {
+                    if(n/i==i)
+                        v.push_back(i);
+                    else
+                    {
+                        v.push_back(i);
+                        v.push_back(n/i);
+                    }
+                }
+     
+            }
+            sort(v.begin(),v.end());
+            for(i = 0; i < v.size(); i++)
+            {
+                if(g==1)break;
+                for(j = i+1; j < v.size(); j++)
+                {
+                    if(g==1)break;
+                    for(k = j+1; k < v.size(); k++)
+                    {
+     
+                        if(v[i]*v[j]*v[k]==n&&(v[i]!=v[j]&&v[j]!=v[k]&&v[i]!=v[k]))
+                        {
+                            a[0]=v[i];
+                            a[1]=v[j];
+                            a[2]=v[k];
+                            g=1;
+                            break;
+                        }
+                    }
+                }
+            }
+            //sort(a,a+3);
+            if(a[0]!=0)
+            {
+                cout<<"YES\n";
+                cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<endl;
+            }
+            else
+                cout<<"NO\n";
+                g=0;
+                v.clear();
+     
+        }
+    }
