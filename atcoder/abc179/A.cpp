9137a27a70dcd4b38e738dab892559a2477bf184
Ahnaf-41M <mdahnaf7@gmail.com>
2020-09-19 18:02:00 +0600

    Add solution for problem `A - Plural Form` Link: https://atcoder.jp/contests/abc179/tasks/abc179_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc179/submissions/16851012

---
+++ atcoder/abc179/A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+#define ff      first
+#define ss      second
+#define pb      push_back
+#define ll      long long
+#define MX      100005
+#define inf     1000000
+#define mod     1000000007
+#define endl    "\n"
+
+#define W(t)            while(t--)
+#define all(v)          v.begin(),v.end()
+#define ZERO(a)         memset(a,0,sizeof(a))
+#define MINUS(a)        memset(a,-1,sizeof(a))
+#define rep(i,a,b)      for(i = a; i < b; i++)
+#define IOS             ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+
+int main()
+{
+	IOS
+
+	ll t,n,i;
+	string s;
+	cin>>s;
+	if(s[s.size()-1]!='s')
+		cout<<s<<"s";
+	else
+		cout<<s<<"es";
+}
