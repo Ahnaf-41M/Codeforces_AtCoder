628ad424c4b8cb01c4c14ea005ea77e0f336e92a
Ahnaf-41M <mdahnaf7@gmail.com>
2020-11-01 18:01:00 +0600

    Add solution for problem `A - Heavy Rotation` Link: https://atcoder.jp/contests/abc181/tasks/abc181_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc181/submissions/17781499

---
+++ atcoder/abc181/A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+#define  ff      first
+#define  ss      second
+#define  pb      push_back
+#define  ll      long long
+#define  MX      100005
+#define  inf     1000000
+#define  mod     1000000007
+#define  endl    "\n"
+
+#define  W(t)            while(t--)
+#define  gcd(a,b)        __gcd(a,b)
+#define  lcm(a,b)        (a*b)/gcd(a,b)
+#define  all(v)          v.begin(),v.end()
+#define  clr(a,x)        memset(a,x,sizeof(a))
+#define  rep(i,a,b)      for(i = a; i < b; i++)
+#define  IOS             ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+using namespace std;
+using namespace __gnu_cxx;
+using namespace __gnu_pbds;
+template <typename T>  using ordered_set =
+   tree<T, null_type, less<T>,
+   rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+
+   IOS
+   ll n;
+   cin >> n;
+   if(n&1)
+      cout << "Black\n";
+   else
+      cout << "White\n";
+
+}
