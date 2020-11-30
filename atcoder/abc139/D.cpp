998d51005571b243850bc3f841a43d3399726c1e
Ahnaf-41M <mdahnaf7@gmail.com>
2020-11-30 13:09:00 +0600

    Add solution for problem `D - ModSum` Link: https://atcoder.jp/contests/abc139/tasks/abc139_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc139/submissions/18499238

---
+++ atcoder/abc139/D.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+
+#define  pb   push_back
+#define  int  long long
+#define  endl "\n"
+#define  MX   100005
+
+#define  W(t)            while(t--)
+#define  all(v)          v.begin(),v.end()
+#define  gcd(a,b)        __gcd(a,b)
+#define  lcm(a,b)        (a*b)/gcd(a,b)
+#define  rep(i,a,b)      for(int i = a; i <= b; i++)
+
+using namespace std;
+using namespace __gnu_cxx;
+
+void solve()
+{
+   int n, ans = 0;
+   cin >> n;
+   ans = n * (n + 1) / 2 - n;
+   cout << ans;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+   solve();
+
+   return 0;
+}
