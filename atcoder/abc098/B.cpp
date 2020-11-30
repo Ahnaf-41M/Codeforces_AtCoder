91b004809391ed2a32d714883aa2bdf4647a0f31
Ahnaf-41M <mdahnaf7@gmail.com>
2020-11-30 17:33:00 +0600

    Add solution for problem `B - Cut and Count` Link: https://atcoder.jp/contests/abc098/tasks/abc098_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc098/submissions/18502569

--- atcoder/abc098/B.cpp
+++ atcoder/abc098/B.cpp
@@ -1,64 +1,46 @@
-#pragma GCC target ("sse4.2")
 #include <bits/stdc++.h>
 #include <ext/rope>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
 
-#define  ff      first
-#define  ss      second
-#define  pb      push_back
-#define  int     long long
-#define  MX      100005
-#define  inf     INT_MAX
-#define  mod     1000000007
-#define  endl    "\n"
+#define  pb   push_back
+#define  int  long long
+#define  endl "\n"
+#define  MX   100005
 
 #define  W(t)            while(t--)
+#define  all(v)          v.begin(),v.end()
 #define  gcd(a,b)        __gcd(a,b)
 #define  lcm(a,b)        (a*b)/gcd(a,b)
-#define  all(v)          v.begin(),v.end()
 #define  rep(i,a,b)      for(int i = a; i <= b; i++)
-#define  irep(i,b,a)     for(int i = b; i >= a; i--)
-#define  IOS             ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
 using namespace std;
 using namespace __gnu_cxx;
-using namespace __gnu_pbds;
-template <typename T>  using ordered_set =
-   tree<T, null_type, less<T>,
-   rb_tree_tag, tree_order_statistics_node_update>;
 
 void solve()
 {
-   ordered_set<char> Q;
-   int n, res = 0;
-   string s;
+   int n, ans = 0;
    map<char, int> freq;
-
+   string s;
 
    cin >> n >> s;
 
-   rep(i, 0, n - 1) freq[s[i]]++;
-
    rep(i, 0, n - 1) {
-      freq[s[i]]--;
-      if (!freq[s[i]])
-         Q.erase(s[i]);
-      else
-         Q.insert(s[i]);
-      
-      int curlen = Q.size();
-      res = max(res, curlen);
+      freq[s[i]]++;
+      int tmp = 0;
+      map<char, int> found;
+      rep(j, i + 1, n - 1) {
+         if (freq[s[j]] && !found[s[j]])
+            tmp++, found[s[j]]++;
+      }
+      ans = max(ans, tmp);
    }
-   cout << res;
+   cout << ans;
+   return;
 }
 signed main()
 {
-//#ifndef ONLINE_JUDGE
-//   freopen("input.txt", "r", stdin);
-//   freopen("output.txt", "w", stdout);
-//#endif
-
-   IOS
+   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
    solve();
+
+   return 0;
 }
