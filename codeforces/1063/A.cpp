2bb15d23bc0053d8da5f673868a825a92a6bebaa
Ahnaf-41M <mdahnaf7@gmail.com>
2021-01-16 10:52:00 +0600

    Add solution for problem `A - Oh Those Palindromes` Link: https://codeforces.com/contest/1063/problem/A Tags: constructive algorithms, strings, *1300 Ref: https://codeforces.com/contest/1063/submission/104464501

---
+++ codeforces/1063/A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+
+#define  pb            push_back
+#define  int           long long
+#define  endl          "\n"
+#define  MX            100005
+
+#define  all(v)        v.begin(),v.end()
+#define  gcd(a,b)      __gcd(a,b)
+#define  lcm(a,b)      (a*b)/gcd(a,b)
+#define  rep(i,a,b)    for(int i = a; i <= b; i++)
+#define  irep(i,b,a)   for(int i = b; i >= a; i--)
+
+using namespace std;
+using namespace __gnu_cxx;
+
+void Solve()
+{
+   int n;
+   string s, ans = "";
+   map<char, int> mp;
+
+   cin >> n >> s;
+   rep(i, 0, n - 1) mp[s[i]]++;
+
+   for (auto it : mp)
+      rep(i, 1, it.second) ans += it.first;
+   cout << ans;
+   return;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   //int T;
+   //cin >> T;
+
+   //while (T--)
+   Solve();
+
+   return 0;
+}
