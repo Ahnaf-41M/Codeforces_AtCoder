82af4fecaf372db596cdc75e480188fe8a89483c
Ahnaf-41M <mdahnaf7@gmail.com>
2021-01-22 21:05:00 +0600

    Add solution for problem `B - New Year's Eve` Link: https://codeforces.com/contest/912/problem/B Tags: bitmasks, constructive algorithms, number theory, *1300 Ref: https://codeforces.com/contest/912/submission/105075040

---
+++ codeforces/912/B.cpp
@@ -0,0 +1,48 @@
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/rope>
+
+#define  pb            push_back
+#define  int           long long
+#define  endl          "\n"
+#define  MX            100005
+
+#define  all(v)        v.begin(),v.end()
+#define  gcd(a,b)      __gcd(a,b)
+#define  lcm(a,b)      (a*b)/gcd(a,b)
+#define  rep(i,a,b)    for(int i = a; i <= b; i++)
+#define  irep(i,b,a)   for(int i = b; i >= a; i--)
+
+using namespace std;
+using namespace __gnu_cxx;
+
+void Solve()
+{
+   int N, K;
+
+   cin >> N >> K;
+
+   if (K == 1)
+      cout << N;
+   else {
+      int cur = 1;
+      while (cur <= N)
+         cur <<= 1;
+      cout << cur - 1;
+   }
+   return;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   //int T;
+   //cin >> T;
+
+   //while (T--)
+   Solve();
+
+   return 0;
+}
