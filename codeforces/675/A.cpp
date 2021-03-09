65a7d0a35ba61727f897d6b26ff7523c91822700
Ahnaf-41M <mdahnaf7@gmail.com>
2021-03-09 08:23:00 +0600

    Add solution for problem `A - Infinite Sequence` Link: https://codeforces.com/contest/675/problem/A Tags: math, *1100 Ref: https://codeforces.com/contest/675/submission/109433625

---
+++ codeforces/675/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+
+#define  pb            push_back
+#define  int           long long
+#define  endl          "\n"
+#define  MX            100005
+
+#define  all(v)        v.begin(),v.end()
+#define  gcd(a,b)      __gcd(a,b)
+#define  lcm(a,b)      (a*b)/gcd(a,b)
+#define  rep(i,a,b)    for(int i = a; i <= b; i++)
+#define  irep(i,b,a)   for(int i = b; i >= a; i--)
+
+using namespace std;
+using namespace __gnu_cxx;
+
+void Solve()
+{
+   int a, b, c;
+   cin >> a >> b >> c;
+
+   if (a == b)
+      cout << "YES\n";
+   else if (c && ((b - a) % c == 0 && ( ((b - a) > 0 && c > 0)
+                                        || ((b - a) < 0 && c < 0))))
+      cout << "YES\n";
+   else
+      cout << "NO\n";
+
+   return;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   //int T;
+   //cin >> T;
+
+   //while (T--)
+   Solve();
+
+   return 0;
+}
