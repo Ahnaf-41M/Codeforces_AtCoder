9f9d049c4fa1dfe15a7a4b8f41c9fed98c387fa1
Ahnaf-41M <mdahnaf7@gmail.com>
2021-04-13 09:10:00 +0600

    Add solution for problem `B - GCD Length` Link: https://codeforces.com/contest/1511/problem/B Tags: constructive algorithms, math, number theory, *1100 Ref: https://codeforces.com/contest/1511/submission/112886451

---
+++ codeforces/1511/B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/rope>
+
+#define  pb            push_back
+#define  int           long long
+#define  endl          "\n"
+#define  MX            100005
+
+#define  all(v)        v.begin(),v.end()
+#define  gcd(a,b)      __gcd(a,b)
+#define  lcm(a,b)      (a*b)/gcd(a,b)
+#define  rep(i,a,b)    for(int i = a; i <= b; i++)
+#define  irep(i,b,a)   for(int i = b; i >= a; i--)
+
+using namespace std;
+using namespace __gnu_cxx;
+
+void Solve()
+{
+   int a, b, c;
+   string s1 = "", s2 = "";
+   cin >> a >> b >> c;
+
+   rep(i, 1, a) s1 += "0";
+   rep(i, 1, b) s2 += "0";
+   s1[0] = '1', s2[0] = '1';
+   s1[a - c] = '1';
+
+   cout << s1 << " " << s2 << endl;
+   return;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   int T;
+   cin >> T;
+
+   while (T--)
+      Solve();
+
+   return 0;
+}
