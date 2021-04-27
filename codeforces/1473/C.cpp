ed73aac781e2117ed80194764096209ea9a592ee
Ahnaf-41M <mdahnaf7@gmail.com>
2021-04-27 21:22:00 +0600

    Add solution for problem `C - No More Inversions` Link: https://codeforces.com/contest/1473/problem/C Tags: constructive algorithms, math, *1500 Ref: https://codeforces.com/contest/1473/submission/114377384

--- codeforces/1473/C.cpp
+++ codeforces/1473/C.cpp
@@ -18,10 +18,25 @@ using namespace __gnu_cxx;
 void Solve()
 {
    int n, k;
+   vector<int> v;
+   stack<int> ans;
+   map<int, int> done;
+
    cin >> n >> k;
-   rep(i, 1, k - (n - k) - 1) cout << i << " ";
-   irep(i, k, k - (n - k)) cout << i << " ";
-   cout << endl;
+
+   rep(i, 1, k) v.pb(i);
+   n -= k;
+   rep(i, 1, n) v.pb(k - i);
+
+   irep(i, (int)v.size() - 1, 0) {
+      if (!done[v[i]]) {
+         ans.push(v[i]);
+         done[v[i]] = 1;
+      }
+   }
+   while (!ans.empty())
+      cout << ans.top() << " ", ans.pop();
+   cout << "\n";
    return;
 }
 signed main()
