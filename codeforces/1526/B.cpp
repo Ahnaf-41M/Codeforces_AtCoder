e5b26a09746c246d426b9cec96791de2f486617d
Ahnaf-41M <mdahnaf7@gmail.com>
2021-05-29 20:31:00 +0600

    Add solution for problem `B - I Hate 1111` Link: https://codeforces.com/contest/1526/problem/B Tags: dp, math, number theory, *1400 Ref: https://codeforces.com/contest/1526/submission/117784356

---
+++ codeforces/1526/B.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  pb           push_back
+#define  MX           100005
+#define  int          long long
+#define  PII          pair<int,int>
+#define  endl         "\n"
+#define  gcd(a,b)     __gcd(a,b)
+#define  lcm(a,b)     (a*b)/gcd(a,b)
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+
+void Solve(int tc)
+{
+   int x;
+   cin >> x;
+
+   if (x > 1099) cout << "YES\n";
+   else {
+      rep(i, 0, 100) {
+         rep(j, 0, 100) {
+            int cur = i * 11 + j * 111;
+            if (cur == x) {
+               cout << "YES\n";
+               return;
+            }
+         }
+      }
+      cout << "NO\n";
+   }
+   return;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   int T = 1;
+   cin >> T;
+
+   for (int tc = 1; tc <= T; tc++) {
+      Solve(tc);
+   }
+
+   return 0;
+}
