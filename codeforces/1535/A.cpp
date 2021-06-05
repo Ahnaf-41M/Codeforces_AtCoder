41289b6d61fa65fe0761d558ba91e1f26191392a
Ahnaf-41M <mdahnaf7@gmail.com>
2021-06-05 09:13:00 +0600

    Add solution for problem `A - Fair Playoff` Link: https://codeforces.com/contest/1535/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1535/submission/118466525

---
+++ codeforces/1535/A.cpp
@@ -0,0 +1,41 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  pb           push_back
+#define  MX           100005
+#define  int          long long
+#define  PII          pair<int,int>
+#define  endl         "\n"
+#define  gcd(a,b)     __gcd(a,b)
+#define  lcm(a,b)     (a*b)/gcd(a,b)
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+   int a, b, c, d;
+   cin >> a >> b >> c >> d;
+
+   if (max(a, b) > min(c, d) && max(c, d) > min(a, b))
+      cout << "YES\n";
+   else
+      cout << "NO\n";
+   return;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   int T = 1;
+   cin >> T;
+
+   for (int tc = 1; tc <= T; tc++) {
+      Solve(tc);
+   }
+
+   return 0;
+}
