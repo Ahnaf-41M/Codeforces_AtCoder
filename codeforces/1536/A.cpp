7dc4f9d5ee75a969e5447e61ebdeabafdd78ead4
Ahnaf-41M <mdahnaf7@gmail.com>
2021-06-07 11:57:00 +0600

    Add solution for problem `A - Omkar and Bad Story` Link: https://codeforces.com/contest/1536/problem/A Tags: brute force, constructive algorithms, *800 Ref: https://codeforces.com/contest/1536/submission/118688542

---
+++ codeforces/1536/A.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<int,int>
+#define  endl         "\n"
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+   int n, ok = 1;
+   cin >> n;
+
+   int ar[n];
+   for (int&x : ar) {
+      cin >> x;
+      if (x < 0) ok = 0;
+   }
+   if (!ok) cout << "NO\n";
+   else {
+      cout << "YES\n101\n";
+      rep(i, 0, 100) cout << i << " ";
+      cout << "\n";
+   }
+   return;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   int T = 1;
+   cin >> T;
+
+   for (int tc = 1; tc <= T; tc++) {
+      Solve(tc);
+   }
+
+   return 0;
+}
