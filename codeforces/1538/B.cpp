0bc5b002ed90457fa233227cd7492c4426eee431
Ahnaf-41M <mdahnaf7@gmail.com>
2021-06-10 20:59:00 +0600

    Add solution for problem `B - Friends and Candies` Link: https://codeforces.com/contest/1538/problem/B Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1538/submission/118992566

---
+++ codeforces/1538/B.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<int,int>
+#define  endl         "\n"
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+	int n, p1, p2, n2, sum = 0;
+	cin >> n;
+
+	int ar[n];
+	for (int &x : ar) {
+		cin >> x;
+		sum += x;
+	}
+	if (sum % n) cout << -1 << "\n";
+	else {
+		int av = sum / n, ans = 0;
+		for (int x : ar) {
+			if (x > av) ans++;
+		}
+		cout << ans << endl;
+	}
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int T = 1;
+	cin >> T;
+
+	for (int tc = 1; tc <= T; tc++) {
+		Solve(tc);
+	}
+
+	return 0;
+}
