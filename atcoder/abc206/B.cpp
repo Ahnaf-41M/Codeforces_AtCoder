d15dbf5e9dbcfbd744fd086a25312b6aa44fdceb
Ahnaf-41M <mdahnaf7@gmail.com>
2021-06-19 18:03:00 +0600

    Add solution for problem `B - Savings` Link: https://atcoder.jp/contests/abc206/tasks/abc206_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc206/submissions/23575010

---
+++ atcoder/abc206/B.cpp
@@ -0,0 +1,42 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<int,int>
+#define  endl         "\n"
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+   int n;
+   cin >> n;
+
+   int ans = 0;
+   rep(i, 1, 1e6) {
+      ans += i;
+      if (ans >= n) {
+         cout << i;
+         return;
+      }
+   }
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+   int T = 1;
+   //cin >> T;
+
+   for (int tc = 1; tc <= T; tc++) {
+      Solve(tc);
+   }
+
+   return 0;
+}
