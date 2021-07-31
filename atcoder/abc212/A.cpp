544ca905af29879b00e3aa2ffa97e37132b919ef
Ahnaf-41M <mdahnaf7@gmail.com>
2021-07-31 18:01:00 +0600

    Add solution for problem `A - Alloy` Link: https://atcoder.jp/contests/abc212/tasks/abc212_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc212/submissions/24645244

---
+++ atcoder/abc212/A.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<int,int>
+#define  endl         "\n"
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+   int a, b;
+   cin >> a >> b;
+   if (a == 0) cout << "Silver";
+   else if (b == 0) cout << "Gold";
+   else cout << "Alloy";
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+
+   int T = 1;
+   //cin >> T;
+
+   for (int tc = 1; tc <= T; tc++) {
+      Solve(tc);
+   }
+
+   return 0;
+}
