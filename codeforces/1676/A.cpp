4536d0d97c92e9f048cd940aa5af8235bf9d77a2
Ahnaf-41M <mdahnaf7@gmail.com>
2022-05-10 20:46:00 +0600

    Add solution for problem `A - Lucky?` Link: https://codeforces.com/contest/1676/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1676/submission/156618688

---
+++ codeforces/1676/A.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<int,int>
+#define  endl         "\n"
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+    string s;
+    cin >> s;
+
+    if (s[0] + s[1] + s[2] == s[3] + s[4] + s[5]) cout << "YES\n";
+    else cout << "NO\n";
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+
+    for (int tc = 1; tc <= T; tc++) {
+        Solve(tc);
+    }
+
+    return 0;
+}
