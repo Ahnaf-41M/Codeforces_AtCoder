d3e8dde1f8bee2290173f5cbc46d1cbf91f23ea6
Ahnaf-41M <mdahnaf7@gmail.com>
2022-06-14 20:36:00 +0600

    Add solution for problem `A - Marathon` Link: https://codeforces.com/contest/1692/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1692/submission/160522724

---
+++ codeforces/1692/A.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<int,int>
+#define  endl         "\n"
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+    int ar[4], cnt = 0;
+    for (int &x : ar) cin >> x;
+    if (ar[0] < ar[1]) cnt++;
+    if (ar[0] < ar[2]) cnt++;
+    if (ar[0] < ar[3]) cnt++;
+
+    cout << cnt << "\n";
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+
+    for (int tc = 1; tc <= T; tc++) {
+        Solve(tc);
+    }
+
+    return 0;
+}
