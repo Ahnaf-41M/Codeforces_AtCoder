23c5da3c2af7635bc1487ddfafd4ad394d8fa648
Ahnaf-41M <mdahnaf7@gmail.com>
2022-10-13 20:38:00 +0600

    Add solution for problem `B - Increasing` Link: https://codeforces.com/contest/1742/problem/B Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/1742/submission/175901410

---
+++ codeforces/1742/B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<int,int>
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+    int n;
+    map<int, int> mp;
+
+    cin >> n;
+    int ok = 1;
+    for (int i = 0; i < n; ++i)
+    {
+        int x; cin >> x;
+        if (mp[x]) ok = 0;
+        mp[x]++;
+    }
+    if (ok) cout << "YES\n";
+    else cout << "NO\n";
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+
+    for (int tc = 1; tc <= T; tc++) {
+        Solve(tc);
+    }
+
+    return 0;
+}
