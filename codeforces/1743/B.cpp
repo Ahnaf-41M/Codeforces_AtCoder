6675ec4111ce27bdc37c11c76beafedfeabb6141
Ahnaf-41M <mdahnaf7@gmail.com>
2022-10-17 20:45:00 +0600

    Add solution for problem `B - Permutation Value` Link: https://codeforces.com/contest/1743/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1743/submission/176711089

---
+++ codeforces/1743/B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<int,int>
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+    int n;
+    cin >> n;
+
+    int i = 1, j = n;
+    while (i <= j) {
+        if (i != j) cout << i << " " << j << " ";
+        else cout << i << " ";
+        i++, j--;
+    }
+    cout << "\n";
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+
+    for (int tc = 1; tc <= T; tc++) {
+        Solve(tc);
+    }
+
+    return 0;
+}
